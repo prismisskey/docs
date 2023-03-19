f51c4fefccc82fec07b696bbf5bc58577dd83955
まっちゃてぃー <56515516+mattyatea@users.noreply.github.com>
2023-03-19 09:44:17 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -29,7 +29,7 @@ const CONFIG = {
   lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ["Blog", "Website", "Notion","mattyatea","misskey","cloudflare","morethan-log","vercel","ミスキー","みすきー","mattyaski","まっちゃてぃー","抹茶ティー","マニュアル","インストール","構築","インスタンス","instance","まっちゃすきー","mattyacocacora"."Misskey","建て方","v13",],
+    keywords: ["Blog", "Website", "Notion","mattyatea","misskey","cloudflare","morethan-log","vercel","ミスキー","みすきー","mattyaski","まっちゃてぃー","抹茶ティー","マニュアル","インストール","構築","インスタンス","instance","まっちゃすきー","mattyacocacora","Misskey","建て方","v13",],
   },
 
   // notion configuration (required)
